825cd4c1b221bc4d2bfa1ca901d5f8ec9eea1f7b
Aluno <Aluno@SN-279231>
2019-02-05 20:29:18 -0200

    Documentado classe IMC

--- src/calculoimc/Imc.java
+++ src/calculoimc/Imc.java
@@ -2,13 +2,36 @@
 package calculoimc;
 
 
+
 public class Imc {
+    
+    
+    public String Situacao;
+
+    //calcula o IMC
+    
     public double calcular(double peso, double altura){
+        
         return peso / (altura*altura);   
     }
+
+    //retorna situacao IMC
     
     
-       
+    
+    public String getSituacao() {
+        if(!this.Situacao.equals("")){
+            
+        } else {
+            return Situacao = ("critica");
+        }
+        return ("Acima do peso ");
+    }
+    
+  
         
+            
+            
+        }
       
-}
+/*}*/
