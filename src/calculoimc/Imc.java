f2f3c5765f3bed1799d1b32e98dc46dd404835e7
Aluno <Aluno@SN-279231>
2019-02-05 19:51:08 -0200

    CRIADO INTERFACE GRÁFICA CRIDAOD A CLASSE DE CAUCULO IMC TRATAMENTO DOS TEXTOS DAS LABELS.

---
+++ src/calculoimc/Imc.java
@@ -0,0 +1,14 @@
+
+package calculoimc;
+
+
+public class Imc {
+    public double calcular(double peso, double altura){
+        return peso / (altura*altura);   
+    }
+    
+    
+       
+        
+      
+}
